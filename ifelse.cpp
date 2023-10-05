8f6ca186f03370b3d1f4677452856dbdd37421ff
bhawana <110031439+bhawana9024@users.noreply.github.com>
2023-10-05 18:24:10 +0530

    Add files via upload
    in this we provide first hello world program in c++

---
+++ ifelse.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a,b;
+    // cin>>a;
+    // cin>>b;
+    // cout<<a+b;
+
+    //if else condition program
+
+    int package;
+    cin>>package;
+    if (package>10)
+    {
+        cout<<"accepted package";
+
+        /* code */
+    }
+    else{
+        cout<<"not accepted";
+    }
+    
+    return 0;
+} // namespace name
