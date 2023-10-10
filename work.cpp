ef182f9bbb152b2e5d150ed65f1c09d46ff1de02
bhawana <110031439+bhawana9024@users.noreply.github.com>
2023-10-10 11:23:35 +0530

    Add files via upload

---
+++ work.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //even and odd check
+ int num;
+    cin>>num;
+    if (num%2==0)
+    {
+        cout<<"num is even";
+        
+    }
+    else{
+cout<<"num is odd";
+    }
+    
+
+    //check -ve, +ve, 0
+
+    /*
+    int num;
+    cin>>num;
+    if(num>0){
+        cout<<"num is posi";
+
+    }
+    else if(num<0){
+        cout<<"num is negi";
+    }
+    else {
+        cout<<"num is 0";
+    }
+
+    */
+ 
